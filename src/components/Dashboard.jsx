d54b36c0f7c9a4c2f2c45cb3fc8eb1a75af7817a
abbas-cedcoss <zaidi.yam5@gmail.com>
2022-05-07 11:16:24 +0530

    debounc

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -40,6 +40,19 @@ function Dashboard() {
         }
     }
 
+    function debounce(func, timeout = 300) {
+        let timer;
+        return (...args) => {
+            if (timer) {
+                func.apply(this, args)
+            }
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                timer = undefined;
+            }, timeout);
+        }
+    }
+
     useEffect(() => {
         getToken();
     });
